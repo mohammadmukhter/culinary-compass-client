364ed502935f99edacb4e315fd3355195513a962
MUKHTER <mohammadmukhter@gmail.com>
2023-06-07 15:30:53 +0600

    register and login form implement with react hook form and implement filed validation

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,18 @@
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import bgImg from "../../assets/banner/pastry2.jpg";
 import PrimaryButton from "../../components/PrimaryButton/PrimaryButton";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="">
       <div
@@ -14,35 +24,45 @@ const Login = () => {
         }}
       >
         <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content">
+        <div className="hero-content text-center text-gray-700">
           <div className="hero min-h-screen bg-transparent">
             <div className="hero-content">
               <div className="card  w-full max-w-sm shadow-2xl bg-base-100/50">
-                <form className="card-body">
+                <h2 className="text-gray-800 text-3xl font-bold mt-6">
+                  Please Login
+                </h2>
+                <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
                   <div className="form-control">
                     <label className="label">
                       <span className="label-text">Email</span>
                     </label>
                     <input
-                      type="text"
+                      type="email"
                       placeholder="email"
+                      {...register("email", { required: true })}
                       className="input input-bordered"
                     />
+                    {errors.email && (
+                      <span className="text-red-600 text-left text-sm">
+                        Email is required
+                      </span>
+                    )}
                   </div>
                   <div className="form-control">
                     <label className="label">
                       <span className="label-text">Password</span>
                     </label>
                     <input
-                      type="text"
+                      type="password"
                       placeholder="password"
+                      {...register("password", { required: true })}
                       className="input input-bordered"
                     />
-                    <label className="label">
-                      <a href="#" className="label-text-alt link link-hover">
-                        Forgot password?
-                      </a>
-                    </label>
+                    {errors.password && (
+                      <span className="text-red-600 text-left text-sm">
+                        Password is required
+                      </span>
+                    )}
                   </div>
                   <div className="form-control mt-6">
                     <PrimaryButton className="btn btn-primary">
